987388e0f955fc732c83117f4cb0e51e067ba914
williamjhyland <williamjhyland@outlook.com>
2025-03-13 16:01:52 -0400

    restructure

---
+++ move.sh
@@ -0,0 +1,5 @@
+# From the repository root (placeholder-sensor), move the contents of placeholder up:
+mv placeholder/* placeholder/.[!.]* .
+# Then remove the now-empty placeholder directory:
+rmdir placeholder
+
